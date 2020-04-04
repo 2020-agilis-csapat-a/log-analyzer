bae26b9e871db34f8b1d8afaa804f8c2c12729f6
Csaba Sarkadi <sarkadicsa@tutanota.de>
2020-04-04 23:17:41 +0200

    logs/parsing: Implement record separation.

---
+++ analyzer/logs/test_parsing.py
@@ -0,0 +1,55 @@
+"""
+Tests for parsing utilities.
+"""
+
+import pytest
+from analyzer.logs.parsing import gather_records
+
+
+@pytest.fixture
+def sample_line() -> str:
+    return ' '.join([
+        '2014/Oct/24',
+        '19:16:48.062933',
+        '111',
+        'SYSCALL',
+        'ExampleComponentTest.ttcn:313(function:ExampleTestedFunction)',
+        'open(0x7F323232) = -1'
+    ])
+
+
+class TestRecordSeparation:
+
+    def test_garbage_yields_no_records(self):
+        counterexamples = [*gather_records("""
+            This does not quite
+            look anything like
+            a log record to parse.
+        """)]
+
+        assert not counterexamples
+
+    def test_yields_valid_single_line_record(self, sample_line):
+        lines = [sample_line]
+        lines_by_record = [*gather_records(lines)]
+
+        assert len(lines_by_record) == 1
+        assert lines_by_record[0] == lines
+
+    def test_yields_valid_multiple_singleline_records(self, sample_line):
+        record_lines = [sample_line]
+        lines_by_record = [*gather_records(record_lines * 3)]
+
+        assert len(lines_by_record) == 3
+        for lines in lines_by_record:
+            assert lines == record_lines
+
+    def test_yields_valid_single_multiline_record(self, sample_line):
+        record_lines = [
+            sample_line,
+            '/me glares at python with malicious intent.'
+        ]
+        lines_by_record = [*gather_records(record_lines)]
+
+        assert len(lines_by_record) == 1
+        assert lines_by_record[0] == record_lines
