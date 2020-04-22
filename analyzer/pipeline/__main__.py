177581df084dc280940b262e9b9f19d9594b53dc
Csaba Sarkadi <sarkadicsa@tutanota.de>
2020-04-22 13:12:33 +0200

    analyzer/pipeline: Simplified syntax for single dependencies.

--- analyzer/pipeline/__main__.py
+++ analyzer/pipeline/__main__.py
@@ -17,6 +17,12 @@ if len(argv) != 2:
     exit(1)
 
 config_yml = yaml.safe_load(open(argv[1], 'rb').read())
+for stage in config_yml.keys():
+    s = config_yml[stage]
+    if 'depends_on' in s:
+        if type(s['depends_on']) == str:
+            config_yml[stage]['depends_on'] = [s['depends_on']]
+
 config = PipelineConfiguration(config_yml)
 pipeline = Pipeline(config)
 
